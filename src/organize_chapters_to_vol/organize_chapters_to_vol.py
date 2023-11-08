3005dd1b320a541b46e486e4cfa260f0fa02518b
Richard Chan <r32095@gmail.com>
2023-11-08 00:14:31 -0600

    made some changes to existing functionality and new feature

--- src/organize_chapters_to_vol/organize_chapters_to_vol.py
+++ src/organize_chapters_to_vol/organize_chapters_to_vol.py
@@ -1,29 +1,64 @@
 from src.exceptions.exceptions import ServiceError, OrganizeChaptersToVolError
-from src.utilities.os_functions import get_files, get_organization_file
+from src.utilities.os_functions import *
 from json import load
+from typing import Iterable
+from re import findall
 
 
-def read_organization_file():
-    """returns content of json organization file"""
-    file = get_organization_file()
+def get_mapping_data():
+    file_name = get_organization_file()
+    file = open(file_name)
     return load(file)
 
 
+def create_mapping_chapters_to_vols():
+    """returns content of json organization file"""
+    schema = get_mapping_data()
+    chapters = schema["chapters"]
+    mapping = {}
+    for chapter in chapters:
+        chapter_number = chapter["chapter"]
+        volume_number = chapter["volume"]
+        mapping[chapter_number] = volume_number
+
+    return mapping
+
+
+def get_chapter_number_from_file(file_name):
+    """function to get chapter number out of file name"""
+    return findall("[0-9]+", file_name)
+
+
+def write_to_system(
+    directory_out: str, chapters: Iterable[str], mapping: dict[str, str]
+):
+    """creates directories for volumes and moves files into those directories"""
+    for chapter in chapters:
+        chapter_name = chapter.name
+        chapter_path = chapter.path
+
+        chapter_number = get_chapter_number_from_file(chapter_name)[0]
+        if chapter_number not in mapping:
+            continue
+        print(f"moving chapter {chapter_number} into folder {mapping[chapter_number]}")
+        volume = mapping[chapter_number]
+        sub_directory = make_sub_directory(directory_out, volume)
+        new_path = f"{sub_directory}/{chapter_name}"
+        move_file(old_path=chapter_path, new_path=new_path)
+
+
 def organize_chapters_to_vol(directory_in: str, directory_out: str):
-    # TODO: read files in directory in
-    files = get_files(directory=directory_in)
-
-    # read organization file
-    schema = read_organization_file()
-
-    # TODO: write folders into directory_out
-    return {}
+    """function to move chapters into corresponding subdirectory folders as volumes"""
+    chapters = get_files(directory=directory_in)
+    mapping = create_mapping_chapters_to_vols()
+    write_to_system(directory_out, chapters, mapping)
 
 
 def main(directory_in: str, directory_out: str):
     """main function for organizing files feature"""
     try:
         organize_chapters_to_vol(directory_in, directory_out)
-    except OrganizeChaptersToVolError as err:
-        print("error occurred when organizing files")
+    except Exception as err:
+        if isinstance(err, OrganizeChaptersToVolError):
+            print("error occurred when organizing files")
         raise ServiceError(err)
--- src/utilities/os_functions.py
+++ src/utilities/os_functions.py
@@ -4,25 +4,37 @@ from typing import Iterable
 from PIL import Image
 
 
-def get_files(directory: str) -> Iterable[str]:
+def get_files(directory: str) -> Iterable:
     """function to get list of files"""
     with os.scandir(directory) as entries:
-        return [entry.name for entry in entries]
+        return [entry for entry in entries]
 
 
-def rename_files(directory_in, directory_out, files):
+def rename_files(directory_out, files):
     """rename files in a directory"""
     story = os.getenv("STORY")
     chapter = os.getenv("CHAPTER")
 
     for page_number, file in enumerate(files):
-        old = f"{directory_in}/{file}"
         new_name = generate_name(story, chapter, page_number)
-        img = Image.open(old)
+        img = Image.open(file)
         img.save(f"{directory_out}/{new_name}")
-        os.remove(old)
+        os.remove(file)
 
 
 def get_organization_file():
     """returns the env variable for the json file to organize volumes and chapters"""
     return os.getenv("ORGANIZATION_FILE")
+
+
+def make_sub_directory(directory_out: str, sub_directory: str):
+    """function to create a sub directory"""
+    sub_directory_path = f"{directory_out}/{sub_directory}"
+    if not os.path.exists(sub_directory_path):
+        os.mkdir(sub_directory_path)
+    return sub_directory_path
+
+
+def move_file(old_path: str, new_path: str):
+    """function to move a file by changing path"""
+    os.rename(old_path, new_path)
