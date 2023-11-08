48f031d3d6e0247eca031bb95b9cb691316323f9
Richard Chan <r32095@gmail.com>
2023-11-08 14:48:33 -0600

    updating file organizer with easier schema

--- src/organize_chapters_to_vol/organize_chapters_to_vol.py
+++ src/organize_chapters_to_vol/organize_chapters_to_vol.py
@@ -4,22 +4,42 @@ from json import load
 from typing import Iterable
 from re import findall
 
+VOLUMES = "volumes"
+VOLUME = "volume"
+START_CHAPTER = "startChapter"
+END_CHAPTER = "endChapter"
 
-def get_mapping_data():
+
+def get_chapters_to_vols_data():
     file_name = get_organization_file()
     file = open(file_name)
     return load(file)
 
 
+def is_valid_chapter(chapter):
+    """function to determine if chapter contains all attributes"""
+    is_with_volume = VOLUME in chapter
+    is_with_start = START_CHAPTER in chapter
+    is_with_end = END_CHAPTER in chapter
+    return all([is_with_end, is_with_start, is_with_volume])
+
+
 def create_mapping_chapters_to_vols():
     """returns content of json organization file"""
-    schema = get_mapping_data()
-    chapters = schema["chapters"]
+    schema = get_chapters_to_vols_data()
+    if "volumes" not in schema:
+        raise OrganizeChaptersToVolError("expected volumes attribute in schema")
+
+    chapters = schema[VOLUMES]
     mapping = {}
     for chapter in chapters:
-        chapter_number = chapter["chapter"]
-        volume_number = chapter["volume"]
-        mapping[chapter_number] = volume_number
+        if not is_valid_chapter(chapter):
+            raise OrganizeChaptersToVolError("volume missing attribute")
+
+        volume = chapter[VOLUME]
+        start_chapter = chapter[START_CHAPTER]
+        end_chapter = chapter[END_CHAPTER]
+        mapping[volume] = (start_chapter, end_chapter)
 
     return mapping
 
@@ -29,22 +49,31 @@ def get_chapter_number_from_file(file_name):
     return findall("[0-9]+", file_name)
 
 
+def write_chapters_for_volume(sub_directory, chapter_details, chapters: Iterable[str]):
+    start_chapter, end_chapter = chapter_details
+    moved_files = []
+    for chapter in chapters:
+        chapter_name = chapter.name
+        chapter_path = chapter.path
+        chapter_number = int(get_chapter_number_from_file(chapter_name)[0])
+        if not (start_chapter <= chapter_number <= end_chapter):
+            continue
+
+        new_path = f"{sub_directory}/{chapter_name}"
+        move_file(old_path=chapter_path, new_path=new_path)
+        moved_files.append(chapter)
+
+    for file in moved_files:
+        chapters.remove(file)
+
+
 def write_to_system(
     directory_out: str, chapters: Iterable[str], mapping: dict[str, str]
 ):
     """creates directories for volumes and moves files into those directories"""
-    for chapter in chapters:
-        chapter_name = chapter.name
-        chapter_path = chapter.path
-
-        chapter_number = get_chapter_number_from_file(chapter_name)[0]
-        if chapter_number not in mapping:
-            continue
-        print(f"moving chapter {chapter_number} into folder {mapping[chapter_number]}")
-        volume = mapping[chapter_number]
+    for volume, chapter_details in mapping.items():
         sub_directory = make_sub_directory(directory_out, volume)
-        new_path = f"{sub_directory}/{chapter_name}"
-        move_file(old_path=chapter_path, new_path=new_path)
+        write_chapters_for_volume(sub_directory, chapter_details, chapters)
 
 
 def organize_chapters_to_vol(directory_in: str, directory_out: str):
--- test/organize_chapters_to_vol/test_organize_chapters_to_vol.py
+++ test/organize_chapters_to_vol/test_organize_chapters_to_vol.py
@@ -7,46 +7,15 @@ from src.organize_chapters_to_vol.organize_chapters_to_vol import (
 @fixture
 def json_file():
     return {
-        "story": "title",
         "volumes": [
-            {"volume": 1, "chapters": [1, 2, 3]},
-            {"volume": 2, "chapters": [4, 5, 6]},
+            {"volume": 1, "chapterStart": 1, "chapterEnd": 3},
+            {"volume": 2, "chapterStart": 4, "chapterEnd": 6},
         ],
     }
 
 
 def test_given_a_list_of_names_and_json_organize_file_names(json_file):
-    # TODO: create mocks
-
-    chapters = [
-        "Bungou Stray Dogs CH - 001 @Manga_Gallery.cbz",
-        "Bungou Stray Dogs CH - 002 @Manga_Gallery.cbz",
-        "Bungou Stray Dogs CH - 003 @Manga_Gallery.cbz",
-        "Bungou Stray Dogs CH - 004 @Manga_Gallery.cbz",
-        "Bungou Stray Dogs CH - 005 @Manga_Gallery.cbz",
-    ]
-
-    expected_organization = [
-        {
-            "volume": 1,
-            "chapters": [
-                "Bungou Stray Dogs CH - 001 @Manga_Gallery.cbz",
-                "Bungou Stray Dogs CH - 002 @Manga_Gallery.cbz",
-                "Bungou Stray Dogs CH - 003 @Manga_Gallery.cbz",
-            ],
-        },
-        {
-            "volume": 2,
-            "chapters": [
-                "Bungou Stray Dogs CH - 004 @Manga_Gallery.cbz",
-                "Bungou Stray Dogs CH - 005 @Manga_Gallery.cbz",
-            ],
-        },
-    ]
-
-    result = organize_chapters_to_vol("", "")
-
-    assert result == expected_organization
+    pass
 
 
 def test_round_partial_chapters_to_nearest_chapter(json_file):
