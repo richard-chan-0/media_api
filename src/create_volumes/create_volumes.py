efd84e8195a3a5841e541f6a789b4130d25bd4fc
Richard Chan <r32095@gmail.com>
2023-11-09 22:43:44 -0600

    added some comments and cleanup logic for empty folders

--- src/create_volumes/create_volumes.py
+++ src/create_volumes/create_volumes.py
@@ -1,6 +1,13 @@
+# using 'import' to resolve circular import issue
 import src.services as AppServices
+
 from src.rezip_chapters_to_vol.rezip_chapters_to_vol import rezip_chapters_to_vol
-from src.utilities.os_functions import get_sub_directories, get_files, move_files
+from src.utilities.os_functions import (
+    get_sub_directories,
+    get_files,
+    move_files,
+    remove_directory,
+)
 import logging
 
 logger = logging.getLogger(__name__)
@@ -29,6 +36,8 @@ def create_volume(services: dict, chaper_files_path: str, volume_name: str):
     move_files(chapters, rezip_service_data.directory_in)
 
     rezip_chapters_to_vol(directory_in, directory_out, volume_name)
+
+    remove_directory(chaper_files_path)
 
 
 def create_volumes():
