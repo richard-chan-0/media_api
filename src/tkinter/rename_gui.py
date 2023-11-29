704779a941f5d9111409265ecf64499a7072ff32
Richard Chan <r32095@gmail.com>
2023-11-28 23:43:30 -0600

    cleaned up gui and added docs

--- src/tkinter/rename_gui.py
+++ src/tkinter/rename_gui.py
@@ -34,7 +34,6 @@ class RenameGui(Gui):
         self.__submit_button_row = 4
         self.__service_message_row = 5
 
-        self.__submit_button = None
         self.__service_message = None
 
         self.__download_label = None
@@ -71,7 +70,7 @@ class RenameGui(Gui):
             self.__root,
             self.__numeric_options,
             self.__numeric_dropdown_row,
-            command=lambda x: self.__update_service_message(f"you selected {x}"),
+            command=lambda x: self.__log_to_console(f"you selected {x}"),
         )
         self.__numeric_click = click
         self.__numeric_dropdown = menu
@@ -81,7 +80,7 @@ class RenameGui(Gui):
             self.__root,
             self.__extension_options,
             self.__extension_dropdown_row,
-            command=lambda x: self.__update_service_message(f"you selected {x}"),
+            command=lambda x: self.__log_to_console(f"you selected {x}"),
         )
         self.__extension_click = click
         self.__extension_dropdown = menu
@@ -99,7 +98,7 @@ class RenameGui(Gui):
         self.__create_extension_dropdown_menu()
 
     def __create_service_dropdown_menu(self, options):
-        click, _ = create_dropdown(
+        create_dropdown(
             self.__root,
             options,
             self.__service_dropdown_row,
@@ -132,18 +131,24 @@ class RenameGui(Gui):
         self.__directory_out = service_metadata.directory_out
         self.__service = service_metadata.service
 
-    def __add_service_widgets(self, value):
-        logger.info("adding download widget to window")
-        self.__update_service_message(f"setting up {value} service")
-        self.__configure_service(value)
-        self.__cleanup()
-        self.__create_download_files_entry()
+    def __create_action_buttons(self):
+        """function to create frame and couple the buttons to the frame on gui"""
         frame = create_frame(
             self.__root, row=self.__submit_button_row, column=COLUMN_COMPONENT
         )
         self.__create_download_button(frame)
         self.__create_submit_button(frame)
         self.__create_rename_button(frame)
+
+    def __add_service_widgets(self, value: str):
+        """function to add widgets based on service selected"""
+        logger.info("adding download widget to window")
+        self.__log_to_console(f"setting up {value} service")
+        self.__configure_service(value)
+        self.__cleanup()
+
+        self.__create_download_files_entry()
+        self.__create_action_buttons()
 
         if value == RENAME_FILES_TO_JELLY_EPISODES:
             self.__create_numeric_dropdown_component()
@@ -155,17 +160,15 @@ class RenameGui(Gui):
     def __pull_files_from_download(self):
         download_path = get_widget_value(self.__download_text)
         if not download_path or not self.__directory_in:
-            self.__update_service_message(
-                "no download directory or destination directory set"
-            )
+            self.__log_to_console("no download directory or destination directory set")
             return
 
         try:
             transfer_files(download_path, self.__directory_in)
-            self.__update_service_message("files have been pulled!")
+            self.__log_to_console("files have been pulled!")
             self.__add_service_widgets()
         except ServiceError as err:
-            self.__update_service_message(str(err))
+            self.__log_to_console(str(err))
 
     def __create_download_button(self, root):
         self.__download_button = create_buttoon(
@@ -186,6 +189,7 @@ class RenameGui(Gui):
         )
 
     def __create_submit_button(self, root):
+        """function to create button for generating name mapping"""
         create_label(self.__root, "Run", row=self.__submit_button_row)
 
         create_buttoon(
@@ -197,6 +201,7 @@ class RenameGui(Gui):
         )
 
     def __create_download_files_entry(self):
+        """function to setup entry component for entering path of download folder"""
         self.__download_label = create_label(
             self.__root, "Download Directory: ", row=self.__download_entry_row
         )
@@ -206,18 +211,20 @@ class RenameGui(Gui):
         self.__download_entry = entry
 
     def __create_story_name_entry(self):
+        """function to setup entry component to enter name for comic functions"""
         create_label(self.__root, "Enter the Story Name:", row=self.__story_name_row)
         text, entry = create_input_field(self.__root, self.__story_name_row)
         self.__story_name_text = text
         self.__story_name_entry = entry
 
-    def __config(self):
+    def __init_gui(self):
+        """function to initialize the gui"""
         logger.info("configuring menu")
         self.__create_window()
 
         options = [service for service in get_list_service() if "rename" in service]
         self.__create_service_dropdown_component(options)
-        self.__create_service_message()
+        self.__create_console_message()
 
     def __create_rename_mapping(self):
         """function that runs rename methods"""
@@ -228,27 +235,31 @@ class RenameGui(Gui):
         service_args.season_number = get_widget_value(self.__numeric_click)
         service_args.story = get_widget_value(self.__story_name_text)
         service_args.extension = get_widget_value(self.__extension_click)
-        logger.info("running service")
+        logger.info("creating name mapping")
 
         self.__rename_mapping = self.__service(service_args)
-        self.__update_service_message(self.__rename_mapping)
+        self.__log_to_console(self.__rename_mapping)
 
     def __update_files(self):
+        """function to rename the files"""
         is_okay = create_confirmation_window(
             "Confirmation", "Are you sure you want to rename these files?"
         )
 
         if not is_okay:
-            self.__update_service_message("rename files aborted!")
+            self.__log_to_console("rename files aborted!")
             return
 
-        self.__update_service_message("renaming files!")
+        logger.info("updating file names in system")
+        self.__log_to_console("renaming files!")
         rename_files(rename_mapping=self.__rename_mapping)
 
-    def __update_service_message(self, message: str):
+    def __log_to_console(self, message: str):
+        """function to update the console window in gui to message"""
         self.__service_message.config(text=message)
 
-    def __create_service_message(self):
+    def __create_console_message(self):
+        """function to create the console window on gui"""
         create_label(self.__root, text="Console", row=self.__service_message_row)
 
         options = {"background": "black", "width": "50"}
@@ -261,5 +272,5 @@ class RenameGui(Gui):
         )
 
     def start(self):
-        self.__config()
+        self.__init_gui()
         self.__root.mainloop()
