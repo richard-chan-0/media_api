d04597949931c32181082dcd45cc9ec8e53c1f4d
Richard Chan <r32095@gmail.com>
2025-03-10 01:37:14 -0500

    replace rename api with formdata instead of json to upload files

--- src/api/rename/comics.py
+++ src/api/rename/comics.py
@@ -1,23 +1,24 @@
-from src.lib.dataclasses import ComicRequest, ServiceArguments, NameChangeRequest
-from src.lib.utilities.os_functions import transfer_files, rename_list_files
+from src.lib.dataclasses import ComicRequest, ServiceArguments
+from src.lib.utilities.os_functions import join_path
 from src.lib.service_constants import IMAGES_IN, IMAGES_OUT
 from src.services.rename_media.rename_media import create_jellyfin_comics_mapping
 
 
-def get_jellyfin_comic_names(request: ComicRequest):
-    transfer_files(request.source, IMAGES_IN)
+def get_jellyfin_video_names_from_files(request):
+    comic_request = ComicRequest(**request.form)
+    files = request.files.getlist("files")
+
+    for file in files:
+        file_path = join_path(IMAGES_IN, file.filename)
+        file.save(file_path)
 
     return create_jellyfin_comics_mapping(
         ServiceArguments(
             **{
                 "directory_in": IMAGES_IN,
                 "directory_out": IMAGES_OUT,
-                "story": request.comic_name,
-                "start_number": request.start_number,
+                "story": comic_request.comic_name,
+                "start_number": comic_request.start_number,
             }
         )
     )
-
-
-def update_comic_names(request_args: NameChangeRequest):
-    rename_list_files(request_args)
--- src/api/rename/routes.py
+++ src/api/rename/routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify
 import logging
-from src.api.rename.videos import get_jellyfin_video_names, update_video_names
-from src.api.rename.comics import get_jellyfin_comic_names, update_comic_names
+from src.api.rename.videos import (
+    get_jellyfin_video_names_from_files,
+)
+from src.api.rename.comics import get_jellyfin_video_names_from_files
 from src.lib.dataclasses.api import (
-    video_request_schema,
-    comic_request_schema,
     name_change_request_schema,
 )
 from src.lib.utilities.app_functions import (
@@ -13,34 +13,28 @@ from src.lib.utilities.app_functions import (
 )
 from src.lib.exceptions.exceptions import ServiceError
 from src.lib.service_constants import BAD_REQUEST_CODE, INTERNAL_SERVER_ERROR_CODE
+from src.lib.utilities.os_functions import rename_list_files
 
 logger = logging.getLogger(__name__)
 rename = Blueprint("rename", __name__, url_prefix="/rename")
 
 function_map = {
-    "videos": (video_request_schema, get_jellyfin_video_names, update_video_names),
-    "comics": (comic_request_schema, get_jellyfin_comic_names, update_comic_names),
+    "videos": get_jellyfin_video_names_from_files,
+    "comics": get_jellyfin_video_names_from_files,
 }
 
 
-def rename_media(media_type):
+def upload_media(media_type: str):
     try:
-        request_schema, get_name, update_name = function_map.get(media_type)
-        if request.method == "GET":
-            logger.info(f"Getting new names for {media_type}...")
-            media_request = check_request_schema(request_schema, request)
-            names = get_name(media_request)
-            return jsonify(convert_to_name_change_request(names))
-
-        logger.info(f"Updating {media_type} names...")
-        name_change_request = check_request_schema(name_change_request_schema, request)
-        update_name(name_change_request)
-        return f"Successfully renamed {media_type} to Jellyfin format."
+        logger.info(f"uploading {media_type}")
+        names = function_map[media_type](request)
+        return jsonify(convert_to_name_change_request(names))
     except ServiceError as e:
-        return str(e), BAD_REQUEST_CODE
+        logger.error(e)
+        return jsonify({"error": str(e)}), BAD_REQUEST_CODE
     except Exception as e:
-        logger.error(f"Error renaming {media_type}: {e}")
-        return f"Error renaming {media_type}.", INTERNAL_SERVER_ERROR_CODE
+        logger.error(e)
+        return jsonify({"error": str(e)}), INTERNAL_SERVER_ERROR_CODE
 
 
 @rename.route("/")
@@ -48,11 +42,19 @@ def hello_world():
     return "Rename Utility API"
 
 
-@rename.route("/videos", methods=["GET", "POST"])
-def rename_videos():
-    return rename_media("videos")
+@rename.route("/process", methods=["POST"])
+def rename_files():
+    logger.info("renaming files")
+    name_change_request = check_request_schema(name_change_request_schema, request)
+    rename_list_files(name_change_request)
+    return "Successfully renamed files."
 
 
-@rename.route("/comics", methods=["GET", "POST"])
-def rename_comics():
-    return rename_media("comics")
+@rename.route("/videos", methods=["POST"])
+def upload_videos():
+    return upload_media("videos")
+
+
+@rename.route("/comics", methods=["POST"])
+def upload_comics():
+    return upload_media("comics")
